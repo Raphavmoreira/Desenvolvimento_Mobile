675e9677c720cca8802f87b9d8a8c5eac2615ddc
Raphael Moreira <92233394+Raphavmoreira@users.noreply.github.com>
2022-12-07 21:52:36 -0300

    Commit

---
+++ Atividade_01/Atividade/src/app/contato-criar/contato-criar.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contato-criar',
+  templateUrl: './contato-criar.page.html',
+  styleUrls: ['./contato-criar.page.scss'],
+})
+export class ContatoCriarPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
